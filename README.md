ad722bdf65163e22d684a345bf73dc5c92c94a97
amass01 <amassarwi@gmail.com>
2023-09-18 14:41:36 +0300

    [APP-1736] Parameterized `dazz-go-lint`.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # dazz-go-lint
+
+`dazz-go-lint-lint` is a pre-commit hook, which can be used to run `golangci-lint`
+with a centralized, unified `.golangci.yml`; in addition, it also runs `go mod tidy`.
+
+## Default params
+
+If not provided the hook uses `https://raw.githubusercontent.com/areionsec/dazz-go-lint/master/.golangci.yml`
+as remote config URL, and `v1.54.2` version of `golangci-lint`.
+
+## Usage
+To add it to your repository, add the following to your `.pre-commit-config.yaml` file:
+```yaml
+repos:
+  - repo: https://github.com/areionsec/dazz-go-lint
+    rev: 1.0.0
+    hooks:
+      - id: dazz-go-lint
+        args: [
+          https://raw.githubusercontent.com/path/to/private/.golangci.yml, # Remote .golangci.yml URL.
+          v1.54.2 # golangci-lint version
+        ]
+```
+
+**Note:** When providing a URL to a private `.golangci.yml`, `dazz-go-lint` 
+requires a `GITHUB_TOKEN` env param, in order to fetch the private yml. The
+env param is not needed when using the default public `.golangci.yml` mentioned
+above, or when providing a URL to a publicly accessible yml.
+
+Then, to run the pre-commit hook command before each commit, run:
+```bash
+pre-commit install
+```
---
+++ lint.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+DEFAULT_CONFIG_FILE_URL="https://raw.githubusercontent.com/areionsec/dazz-go-lint/master/.golangci.yml"
+DEFAULT_GOLANGCI_LINT_VERSION="v1.54.2"
+REMOTE_CONFIG_FILE_URL="$1"
+GOLANGCI_LINT_VERSION="$2"
+
+if [[ -z "$REMOTE_CONFIG_FILE_URL" ]]; then
+   # No custom remote config URL, set to default
+   echo "No remote config URL is provided, using: $DEFAULT_CONFIG_FILE_URL"
+   REMOTE_CONFIG_FILE_URL=$DEFAULT_CONFIG_FILE_URL
+else
+  echo "Using provided remote config URL: $REMOTE_CONFIG_FILE_URL"
+fi
+
+if [[ -z "$GOLANGCI_LINT_VERSION" ]]; then
+   # No custom remote config URL, set to default
+   echo "No 'golangci-lint' version is provided, using: $DEFAULT_GOLANGCI_LINT_VERSION"
+   GOLANGCI_LINT_VERSION=DEFAULT_GOLANGCI_LINT_VERSION
+else
+  echo "Using provided golangci-lint version: $GOLANGCI_LINT_VERSION"
+fi
+
+if [ "$($(go env GOPATH)/bin/golangci-lint version | awk '{print $4}' )" != $GOLANGCI_LINT_VERSION ]; then
+    echo "[+] Installing golangci-lint"
+    curl -sSfL $ https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin $GOLANGCI_LINT_VERSION
+fi
+
+echo "[+] Running golangci-lint"
+
+curl -H "Authorization: token $GITHUB_TOKEN" -L $REMOTE_CONFIG_FILE_URL
+
+$(go env GOPATH)/bin/golangci-lint run -v
+
+echo "[+] Running go mod tidy"
+
+go mod tidy -v
+if [ $? -ne 0 ]; then
+  exit 2
+fi
+
+git diff --exit-code go.* &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "go.mod or go.sum differs, please re-add it to your commit"
+    exit 3
+fi
